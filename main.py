1b333f31e067dbbcf97b9c756fdc90606cab0552
Aman Mondal <111458684+fhjkjjjjj@users.noreply.github.com>
2024-02-12 15:12:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-from flask import Flask, request
-from fastapi import FastAPI, BackgroundTasks, HTTPException
-from pydantic import BaseModel
-from extract import *
-import os
+from flask import Flask
 app = Flask(__name__)
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    if request.method == "GET":
-        return "hello"
+
+@app.route('/')
+def hello_world():
+    return 'GreyMatters'
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
