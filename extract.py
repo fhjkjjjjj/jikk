534d4daed2c75aa55525c36ad4bdf56ba269a668
Aman Mondal <111458684+fhjkjjjjj@users.noreply.github.com>
2024-02-12 14:40:33 +0530

    Update extract.py

--- extract.py
+++ extract.py
@@ -2,6 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
 def get_reward(wallet):
  chrome_options = webdriver.ChromeOptions()
  chrome_options.add_argument("--headless")
