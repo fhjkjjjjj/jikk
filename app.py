151992d4ae5bdcde61d41bb836eff874d9c355b7
Aman Mondal <111458684+fhjkjjjjj@users.noreply.github.com>
2024-03-19 17:43:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ async def get_homepage_data(wallet: str = Query(...)):
     print(wallet)
     url = "tgWebAppData=query_id%3DAAEk6cFwAAAAACTpwXB3CjFn%26user%3D%257B%2522id%2522%253A1891756324%252C%2522first_name%2522%253A%2522Keshav%2522%252C%2522last_name%2522%253A%2522%2522%252C%2522username%2522%253A%2522Keshav881%2522%252C%2522language_code%2522%253A%2522en%2522%252C%2522allows_write_to_pm%2522%253Atrue%257D%26auth_date%3D1710842002%26hash%3De12315fd63e1d2ed969f15e427e2f7d4d8f7f8c781563856259546997fa8c4d1"
   #  wallet_main = "maze property cradle grace only staff opinion purchase discover swallow limb animal"
-    data = get_reward(url, wallet_main) 
+    data = get_reward(url, wallet) 
     return data
 
 
